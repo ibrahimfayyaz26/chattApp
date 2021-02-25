ba1582eb1df217cb42e1928b4e233a0f8b6fd613
Ibrahim <79158349+ibrahimfayyaz26@users.noreply.github.com>
2021-02-25 20:42:28 +0500

    Add files via upload

---
+++ screens/LoginScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, Input, Image } from "react-native-elements";
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>Login Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default LoginScreen;
